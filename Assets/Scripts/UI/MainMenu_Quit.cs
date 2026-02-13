3212bf40c305695b3c36acbc5876e5c8a0047f15
Kslaar <paul.gebert01@gmail.com>
2026-02-13 11:32:10 +0100

    c

--- Assets/Scripts/UI/MainMenu_Quit.cs
+++ Assets/Scripts/UI/MainMenu_Quit.cs
@@ -4,10 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu_Quit : MonoBehaviour
 {
+    // public static MainMenu_Quit Instance { get; private set;}
     [SerializeField] private string mainMenuScene = "MainMenuScene";
 
     [SerializeField] private float minLoadingSeconds = 0.5f;
 
+    /*
+    void Awake()
+    {
+        if (Instance != null) { Destroy(gameObject); return; }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    */
     public void BackToMainMenu()
     {
         // StartCoroutine(BackRoutine());
